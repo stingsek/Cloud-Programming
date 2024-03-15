bbd83d09a80f11314cc57bd3d05e758064e796e6
stingsek <michalzadelek10@gmail.com>
2024-03-15 03:14:07 +0100

    backend done

--- tictactoe/src/main/kotlin/com/cloud_programming/tictactoe/app/TictactoeApplication.kt
+++ tictactoe/src/main/kotlin/com/cloud_programming/tictactoe/app/TictactoeApplication.kt
@@ -1,4 +1,4 @@
-package com.cloud_programming.tictactoe
+package com.cloud_programming.tictactoe.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
